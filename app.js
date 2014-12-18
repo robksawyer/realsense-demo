f6cefa5c6e0969e69ce046f6935bd01a53c58e00
robksawyer <robksawyer@gmail.com>
2014-12-18 14:58:13 -0800

    More progress

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var tests = require('./routes/tests');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 
 
 // uncomment after placing your favicon in /public
@@ -25,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/tests', tests);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -55,12 +58,6 @@ app.use(function(err, req, res, next) {
         message: err.message,
         error: {}
     });
-});
-
-
-app.get('/test', function (req, res)
-{
-    res.render('face_tracking.html');
 });
 
 module.exports = app;
