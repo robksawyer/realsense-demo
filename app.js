fd3c88fb9e738ae96b1f1f3c4bb7b2fe90913981
robksawyer <robksawyer@gmail.com>
2014-12-18 16:19:44 -0800

    Fixed a bug

--- app.js
+++ app.js
@@ -59,11 +59,4 @@ app.use(function(err, req, res, next) {
     });
 });
 
-// bind the app to listen for connections on a specified port
-var port = process.env.PORT || 3001;
-app.listen(port);
-
-// Render some console log output
-console.log("Listening on port " + port);
-
 module.exports = app;
